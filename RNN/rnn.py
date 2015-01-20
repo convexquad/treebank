b4e9bc323319a5ec2397b42a1dbde07b9b1d1e49
Alex Bain <abain@linkedin.com>
2015-01-19 16:10:19 -0800

    Initial RNN backprop implementation for training

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -7,38 +7,213 @@ Written by Alex Bain (https://github.com/convexquad/treebank)
 import numpy
 import treebank
 
+D = 30          # Dimensionality of our word model
+R = 0.0001      # Range for the uniform dist to initialize the words
+C = treebank.C  # Number of Treebank label classes
+E = 1e-12       # Error epsilon for comparing float values to constants
+
+# Fix numpy's random number generation for reproducible results
+numpy.random.seed(10)
+
 # Simple RNN class for Treebank sentiment analysis.
 class RNN:
-   def __init__(self, tbank):
-       self.tbank = tbank
-       self.word_vector_map = tbank.word_vector_map
-       self.W = numpy.zeros((D, 2*D))
-       self.b = numpy.zeros(D)
+    def __init__(self, tbank, alpha, epochs):
+        self.alpha = alpha
+        self.epochs = epochs
+        self.tbank = tbank
+	self.L = numpy.random.uniform(-R, R, (D, len(tbank.vocabulary)))
+        self.W = numpy.random.normal(0, 0.01, (D, 2*D + 1))
+        self.Ws = numpy.random.normal(0, 0.01, (C, D))
+        # self.W = numpy.zeros((D, 2*D + 1))
+        # self.Ws = numpy.zeros((C, D))
 
-   # Makes a forward evaluation pass for a Treebank sentence.
-   def forward_pass(self, tree):
-       if tree.is_leaf():
-           return self.word_vector_map[tree.word]
-       else:
-           p1 = self.forward_pass(tree.left)
-           p2 = self.forward_pass(tree.right)
-           return numpy.tanh(self.W.dot(numpy.concatenate((p1, p2))) + self.b)
+    # Makes a forward evaluation pass for a Treebank sentence.
+    def forward_pass(self, node):
+        if node.is_leaf():
+            node.x_k = self.L[:, node.word_index]
+            assert len(node.x_k) == D
 
+            node.y_k = softmax(self.Ws.dot(node.x_k))
+            assert len(node.y_k) == C
+            assert abs(node.y_k.sum() - 1) < E
+            return node.x_k
+        else:
+            x_l = self.forward_pass(node.left)
+            x_r = self.forward_pass(node.right)
+            assert len(x_l) == D
+            assert len(x_r) == D
+
+            node.z_k = numpy.concatenate((x_l, x_r, numpy.ones((1))))
+            node.a_k = self.W.dot(node.z_k)
+            node.x_k = numpy.tanh(node.a_k)
+            assert len(node.x_k) == D
+
+            node.y_k = softmax(self.Ws.dot(node.x_k))
+            assert len(node.y_k) == C
+            assert abs(node.y_k.sum() - 1) < E
+            return node.x_k
+
+    # Top-level function to train the RNN.
+    def train(self):
+        for i in range(self.epochs):
+            self.training_run(i + 1)
+
+    # Makes an training run on the RNN for a particular training epoch.
+    def training_run(self, epoch):
+        J = 0
+        for tree in self.tbank.dev:
+            self.forward_pass(tree)
+            J = J + self.compute_softmax_err(tree)
+
+        print "Cost in epoch {} is J = {}; matrix norms are W = {}, Ws = {}, L = {}".format(
+            epoch, J, numpy.linalg.norm(self.W), numpy.linalg.norm(self.Ws), numpy.linalg.norm(self.L))
+
+        W_l = self.W[:, 0:D]
+        W_r = self.W[:, D:2*D]
+        assert W_l.shape == (D, D)
+        assert W_r.shape == (D, D)
+
+        # Compute the backprop deltas
+        for tree in self.tbank.dev:
+            self.compute_softmax_delta(tree)
+            self.compute_incoming_delta(tree, W_l, W_r)
+
+        # Compute the backprop gradients
+        grad_L  = numpy.zeros(self.L.shape)
+        grad_W  = numpy.zeros(self.W.shape)
+        grad_Ws = numpy.zeros(self.Ws.shape)
+
+        for tree in self.tbank.dev:
+            self.compute_backprop_L(tree, grad_L, W_l, W_r)
+            self.compute_backprop_W(tree, grad_W)
+            self.compute_backprop_Ws(tree, grad_Ws)
+
+        # Average the word gradients by the word occurrences
+        for word in self.tbank.vocabulary:
+            word_count = self.tbank.word_count_map[word]
+            word_index = self.tbank.word_index_map[word]
+            grad_L[:, word_index] /= word_count
+
+        # Average the W gradients by the number of span nodes
+        grad_W = (1.0 / self.tbank.span_node_count) * grad_W
+
+        # Average the Ws gradients by the total number of nodes
+        grad_Ws = (1.0 / self.tbank.total_node_count) * grad_Ws
+
+        # Make the parameter update
+        self.L  = self.L  - ((self.alpha / epoch) * grad_L)
+        self.W  = self.W  - ((self.alpha / epoch) * grad_W)
+        self.Ws = self.Ws - ((self.alpha / epoch) * grad_Ws)
+
+    # When we compute the softmax cross-entropy cost, take advantage of the
+    # fact that there is only one non-zero term in the sum.
+    def compute_softmax_err(self, node):
+        node.J_ce_k = -numpy.log2(node.y_k[node.label])
+        assert node.J_ce_k >= 0
+        if node.is_leaf():
+            return node.J_ce_k
+        else:
+            J_ce_lk = self.compute_softmax_err(node.left)
+            J_ce_rk = self.compute_softmax_err(node.right)
+            return (node.J_ce_k + J_ce_lk + J_ce_rk)
+
+    # Compute the softmax error deltas for nodes of the tree.
+    def compute_softmax_delta(self, node):
+        if not node.is_leaf():
+            node.delta_sm_k = self.Ws.T.dot(node.y_k - node.t_k) * tanh_prime(node.a_k)
+            assert len(node.delta_sm_k) == D
+            self.compute_softmax_delta(node.left)
+            self.compute_softmax_delta(node.right)
+
+    # Compute the "incoming" error deltas for the nodes in this tree. There is
+    # no incoming delta for the root node, so just recurse into the children.
+    def compute_incoming_delta(self, root, W_l, W_r):
+        root.delta_k = root.delta_sm_k
+        if not root.is_leaf():
+            self.compute_incoming_delta_child(root, root.left, W_l, W_l, W_r)
+            self.compute_incoming_delta_child(root, root.right, W_r, W_l, W_r)
+
+    # Compute the incoming error deltas for non-root nodes of the tree.
+    def compute_incoming_delta_child(self, parent, node, W_k, W_l, W_r):
+        if not node.is_leaf():
+            node.delta_in_k = parent.delta_k.T.dot(W_k).T * tanh_prime(node.a_k)
+            assert len(node.delta_in_k) == D
+            node.delta_k = node.delta_in_k + node.delta_sm_k
+            self.compute_incoming_delta_child(node, node.left, W_l, W_l, W_r)
+            self.compute_incoming_delta_child(node, node.right, W_r, W_l, W_r)
+
+    # Computes the contribution to partial J / partial L for a tree.
+    def compute_backprop_L(self, node, grad_L, W_l, W_r):
+        if node.is_leaf():
+            return
+
+        if node.left.is_leaf():
+            grad_x_n = node.delta_k.T.dot(W_l).T
+            assert len(grad_x_n) == D
+            grad_L[:, node.left.word_index] += grad_x_n
+        else:
+            self.compute_backprop_L(node.left, grad_L, W_l, W_r)
+
+        if node.right.is_leaf():
+            grad_x_n = node.delta_k.T.dot(W_r).T
+            assert len(grad_x_n) == D
+            grad_L[:, node.right.word_index] += grad_x_n
+        else:
+            self.compute_backprop_L(node.right, grad_L, W_l, W_r)
+
+    # Computes the contribution to partial J / partial W for a tree.
+    def compute_backprop_W(self, node, grad_W):
+        if node.is_leaf():
+            return
+        else:
+            grad_k = numpy.outer(node.delta_k, node.z_k)
+            assert grad_k.shape == self.W.shape
+            grad_W += grad_k
+            self.compute_backprop_W(node.left, grad_W)
+            self.compute_backprop_W(node.right, grad_W)
+
+    # Computes the contribution to partial J / partial Ws for a tree.
+    def compute_backprop_Ws(self, node, grad_Ws):
+        grad_k = numpy.outer((node.y_k - node.t_k), node.x_k)
+        assert grad_k.shape == (C, D)
+        grad_Ws += grad_k
+        if not node.is_leaf():
+            self.compute_backprop_Ws(node.left, grad_Ws)
+            self.compute_backprop_Ws(node.right, grad_Ws)
+
+    def show_word_results(self):
+        for word in self.tbank.vocabulary:
+            word_index = self.tbank.word_index_map[word]
+            word_label = self.tbank.word_label_map[word]
+            word_vectr = self.L[:, word_index]
+            prediction = softmax(self.Ws.dot(word_vectr))
+            print "WORD: {}, label: {}, prediction: {}".format(word, word_label, prediction[word_label])
+
+"""
+Note that the normal softmax function is susceptible to overflow. To avoid this,
+subtract a const from each term of the input array before computing exp.
+
+References:
+http://ufldl.stanford.edu/wiki/index.php/Exercise:Softmax_Regression
+http://deeplearning.net/software/theano/library/tensor/nnet/nnet.html#tensor.nnet.softmax
+"""
+def softmax(w):
+    e_x = numpy.exp(w - w.max())
+    dist = e_x / numpy.sum(e_x)
+    return dist
+
+# Derivative of the hyperbolic tangent function (applied elementwise).
+def tanh_prime(x):
+    return (1.0 - numpy.power(numpy.tanh(x), 2))
+
+# Main function for the RNN.
 def main():
+    alpha = 10.0
+    epochs = 30
     tbank = treebank.build_standard_treebank()
-    rnn = RNN(tbank)
-    for tree in tbank.dev:
-        rnn.forward_pass(tree)
+    rnn = RNN(tbank, alpha, epochs)
+    rnn.train()
+    rnn.show_word_results()
    
-    """ 
-    lr = build_softmax_classifier(word_map)
-    (accuracy, predictions, labels) = test_softmax_classifier(lr, word_map)
-    classes = [0 for x in range(5)]
-    for predict in predictions:
-        classes[predict] = classes[predict] + 1
-    print accuracy
-    print classes
-    """
-
 if __name__ == "__main__":
     main()
--- RNN/treebank.py
+++ RNN/treebank.py
@@ -7,8 +7,7 @@ Written by Alex Bain (https://github.com/convexquad/treebank)
 import numpy
 import re
 
-D = 100     # Dimensionality of our word model
-R = 0.0001  # Range for the uniform dist to initialize the words
+C = 5  # Number of Treebank label classes
 
 # Wrapper class for the Treebank datasets. Uses the "all" dataset to build the
 # vocabulary.
@@ -17,17 +16,82 @@ class Treebank:
         self.dev = dev
         self.test = test
         self.train = train
-        self.word_label_map = self.read_word_labels(all)
+        self.word_count_map = self.read_word_counts(dev)
+        self.word_label_map = self.read_word_labels(dev)
         self.vocabulary = self.word_label_map.keys()
-        self.word_vector_map = self.build_word_vector_map(self.vocabulary)
+        self.word_index_map = self.build_word_index_map(self.vocabulary)
+        self.leaf_node_count = self.count_leaf_nodes(dev)
+        self.span_node_count = self.count_span_nodes(dev)
+        self.total_node_count = self.leaf_node_count + self.span_node_count
+        self.add_word_index_trees(dev)
 
-    # Builds a uni(-R, R) vector for each word in the vocabulary
-    def build_word_vector_map(self, vocabulary):
-       word_vector_map = dict()
-       for word in vocabulary:
-           word_vector = numpy.random.uniform(-R, R, D)
-           word_vector_map[word] = word_vector
-       return word_vector_map
+    # Annotate each leaf node in a tree with an index that identifies its word
+    def add_word_index_trees(self, trees):
+        for tree in trees:
+            self.add_word_index_tree(tree)
+
+    # Helper function to annotate the leaf nodes in a tree with an idnex that
+    # identifies its word
+    def add_word_index_tree(self, tree):
+        if tree.is_leaf():
+            word_index = self.word_index_map[tree.word]
+            assert word_index >= 0
+            tree.word_index = word_index
+        else:
+            self.add_word_index_tree(tree.left)
+            self.add_word_index_tree(tree.right)
+
+    # Builds a map of each word to an index identifying that word
+    def build_word_index_map(self, vocabulary):
+        word_index_map = dict()
+        current_index = 0
+        for word in vocabulary:
+            word_index_map[word] = current_index
+            current_index = current_index + 1
+            assert len(word_index_map) == current_index
+        return word_index_map
+
+    def count_leaf_nodes(self, trees):
+        count = 0
+        for tree in trees:
+            count = count + self.count_leaf_nodes_tree(tree)
+        return count
+
+    def count_leaf_nodes_tree(self, tree):
+        if tree.is_leaf():
+            return 1
+        else:
+            return (self.count_leaf_nodes_tree(tree.left) + self.count_leaf_nodes_tree(tree.right))
+
+    def count_span_nodes(self, trees):
+        count = 0
+        for tree in trees:
+            count = count + self.count_span_nodes_tree(tree)
+        return count
+
+    def count_span_nodes_tree(self, tree):
+        if tree.is_leaf():
+            return 0
+        else:
+            return (1 + self.count_span_nodes_tree(tree.left) + self.count_span_nodes_tree(tree.right))
+
+    # Function to build a map of each word to the number of times it appears.
+    def read_word_counts(self, trees):
+        word_count_map = dict()
+        for tree in trees:
+            self.read_word_counts_tree(tree, word_count_map)
+        return word_count_map
+
+    # Helper function to count the number of times each word appears.
+    def read_word_counts_tree(self, tree, word_count_map):
+        if tree.is_leaf():
+            if word_count_map.has_key(tree.word):
+                word_count_map[tree.word] = word_count_map[tree.word] + 1
+            else:
+                word_count_map[tree.word] = 1
+        else:
+            self.read_word_counts_tree(tree.left, word_count_map)
+            self.read_word_counts_tree(tree.right, word_count_map)
 
     # Function to get the words and labels for a set of trees from Treebank.
     def read_word_labels(self, trees):
@@ -36,16 +100,15 @@ class Treebank:
             self.read_word_labels_tree(tree, word_label_map)
         return word_label_map
 
-    # Helper function to add each (word, sentiment) pair in Treebank to a map.
+    # Helper function to add each (word, label) pair in Treebank to a map.
     def read_word_labels_tree(self, tree, word_label_map):
         if tree.is_leaf():
             if word_label_map.has_key(tree.word):
                 # Make a check to see that Treebank always labels the same word
                 # with the same sentiment label.
-                if word_label_map[tree.word] != tree.sentiment:
-                    print "Found word {0} with label {1} and {2}".format(tree.word, word_label_map[tree.word], tree.sentiment)
+                assert word_label_map[tree.word] == tree.label
             else:
-                word_label_map[tree.word] = tree.sentiment
+                word_label_map[tree.word] = tree.label
         else:
             self.read_word_labels_tree(tree.left, word_label_map)
             self.read_word_labels_tree(tree.right, word_label_map)
@@ -53,10 +116,20 @@ class Treebank:
 # Simple Node class for Treebank sentences.
 class Node:
     def __init__(self):
-        self.sentiment = None
         self.word = None
+        self.word_index = None
+        self.label = None
         self.left = None
         self.right = None
+        self.t_k = numpy.zeros(C)  # Vector t_k for the label at node k
+        self.y_k = None            # Vector y_k for the softmax prediction node k
+        self.a_k = None            # Vector a_k at node k
+        self.x_k = None            # Vector x_k at node k
+        self.z_k = None            # Vector z_k at node k
+        self.J_ce_k = 0            # Softmax cross-entropy cost at node k
+        self.delta_sm_k = None     # Softmax error delta at node k
+        self.delta_in_k = None     # "Incoming" error delta at span node k
+        self.delta_k = None        # Full error delta at span node k = delta_in_k + delta_sm_k
 
     def is_leaf(self):
         return (self.word != None)
@@ -65,9 +138,9 @@ class Node:
     # Treebank and diffing our version against the input file.
     def __str__(self):
         if self.is_leaf():
-            return "({0} {1})".format(self.sentiment, self.word)
+            return "({0} {1})".format(self.label, self.word)
         else:
-            return "({0} {1} {2})".format(self.sentiment, self.left, self.right)
+            return "({0} {1} {2})".format(self.label, self.left, self.right)
 
 # Reads all the sentences of a Treebank file our Node representation.
 def read_treebank_file(file_path):
@@ -101,8 +174,9 @@ def tokenize_sentence(line):
                parent.right = current
            current = parent
        else:
-           if current.sentiment == None:
-               current.sentiment = int(token)
+           if current.label == None:
+               current.label = int(token)
+               current.t_k[current.label] = 1.0
            else:
                current.word = token
 
@@ -111,7 +185,7 @@ def build_standard_treebank():
     dev = read_treebank_file("/Users/abain/RNN/trees/dev.txt")
     tst = None # read_treebank_file("/Users/abain/RNN/trees/test.txt")
     trn = None # read_treebank_file("/Users/abain/RNN/trees/train.txt")
-    all = read_treebank_file("/Users/abain/RNN/trees/dev.txt")
+    all = None # read_treebank_file("/Users/abain/RNN/trees/dev.txt")
     return Treebank(dev, tst, trn, all)
 
 # Main function for testing the Treebank parser. Note that we can easily unit
